353e22846f4488a3db217f65b80b4098d0fd096d
EuvhenRight <ugnivenko.ea@gmail.com>
2023-06-13 23:30:15 +0200

    Finished

--- Week1/HomeWork_week_1/app.js
+++ Week1/HomeWork_week_1/app.js
@@ -1,103 +1,41 @@
 const db = require('./connection.js');
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3000
 
 //create a database
 
-    app.get('/createdb', (req, res) => {
-
-// refresh database
-
-    db.query('DROP DATABASE IF EXISTS meetup',(error) => {
-        if (error) {
-        console.log(error);
-        res.status(500).send("An error occurred while dropping the database.");
-        return;
-        }
-        console.log('Connected to the MySQL server.')
+    db.query('DROP DATABASE IF EXISTS meetup',(err) => {
+        if (err) throw err
+        console.log('Database dropped');
     });
 
-    const createSqlDb = `CREATE DATABASE IF NOT EXISTS meetup`;
-    db.query(createSqlDb, (error, result) => {
-        if(error) {
-            console.log(error);
-            res.status(500).send("An error occurred while creating the database.");
-            return;
-        }
-       console.log('DataBase created', result);
-       res.send(result);
-    })
+const createSqlDb = `CREATE DATABASE IF NOT EXISTS meetup`;
+    db.query(createSqlDb, (err) => {
+        if(err) throw err 
+            console.log('Database created')
+    });
 
 // Select the database
 
-    db.query('USE meetup', (error) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send("An error occurred while selecting the database.");
-            return;
-        }
-    
-        console.log('Database selected.');
+    db.query('USE meetup', (err) => {
+    if(err) throw err 
+            console.log('Database selected.');
     });
-})
 
 // Create table Invitee
-    const inviteeTableQuery = `
+const inviteeTableQuery = `
     CREATE TABLE IF NOT EXISTS Invitee (
         invitee_no INT PRIMARY KEY AUTO_INCREMENT,
         invitee_name VARCHAR(255),
         invitee_by VARCHAR(255)
-    )`;
+    )
+    `;
 
-    app.get('/createTableInvitee', (req, res) => {
-    db.query(inviteeTableQuery, (error, result) => {
-        if(error) {
-            console.log(error);
-            res.status(500).send("An error occurred while creating the inviteeTableQuery.");
-            return;
-        }
-    console.log('Invitee Table created', result);
-    res.send(result);
-    })
-    });
-
-// Insert values into Invitee
-
-    const insertValuesInvitee = `INSERT INTO Invitee (invitee_name, invitee_by) 
-    VALUES ('PERSON_1', 'COMPANY_1'), ('PERSON_2', 'COMPANY_2'), ('PERSON_3', 'COMPANY_3'), 
-    ('PERSON_4', 'COMPANY_4'), ('PERSON_5', 'COMPANY_5')`;
-
-    app.get('/insertValuesInvitee', (req, res) => {
-        db.query(insertValuesInvitee, (error, result) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("An error occurred while inserting the values.");
-                return;
-            }
-        console.log('Invitee values added', result);
-        res.send(result);
-        });
-    });
-
-//Show table Invitee
-
-    app.get('/showTableInvitee', (req, res) => {
-        const selectQuery = `SELECT * FROM Invitee`;
-        db.query(selectQuery, (error, result) => {
-            if (error) {
-                console.log(error);
-                res.status(500).send("An error occurred while retrieving table data.");
-                return;
-            }
-            console.log('Table invitee retrieved', result);
-            res.send(result);
-        });
+    db.query(inviteeTableQuery, (err) => {
+        if(err) throw err 
+            console.log('Invitee Table created');
     });
 
 // Create table Room
-    const roomTableQuery = `
+const roomTableQuery = `
     CREATE TABLE IF NOT EXISTS Room (
         room_no INT PRIMARY KEY AUTO_INCREMENT,
         room_name VARCHAR(255),
@@ -105,53 +43,13 @@ const port = process.env.PORT || 3000
     )
     `;
 
-    app.get('/createTableRoom', (req, res) => {
-        db.query(roomTableQuery, (error, result) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("An error occurred while creating the inviteeTableQuery.");
-                return;
-            }
-        console.log('Room Table created', result);
-        res.send(result);
-        })
+    db.query(roomTableQuery, (err) => {
+    if(err) throw err
+        console.log('Room Table created');
         });
 
-// Insert values into Room
-
-    const insertValuesRoom = `INSERT INTO Room (room_name, floor_number)
-    VALUES ('ROOM_1', 1), ('ROOM_2', 2), ('ROOM_3', 3), ('ROOM_4', 4), ('ROOM_5', 5)`;
-
-    app.get('/insertValuesRoom', (req, res) => {
-        db.query(insertValuesRoom, (error, result) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("An error occurred while inserting the values.");
-                return;
-            }
-        console.log('Room values added', result);
-        res.send(result);
-        });
-    })
-
-// Show table Room
-
-    app.get('/showTableRoom', (req, res) => {
-        const selectQuery = `SELECT * FROM Room`;
-        db.query(selectQuery, (error, result) => {
-            if (error) {
-                console.log(error);
-                res.status(500).send("An error occurred while retrieving table data.");
-                return;
-            }
-            console.log('Table room retrieved' , result);
-            res.send(result);
-        });
-    })
-    
 // Create table Meeting
-
-    const meetingTableQuery = `
+const meetingTableQuery = `
     CREATE TABLE IF NOT EXISTS Meeting (
         meeting_no INT PRIMARY KEY AUTO_INCREMENT,
         meeting_title VARCHAR(255),
@@ -162,54 +60,80 @@ const port = process.env.PORT || 3000
     )
     `;
 
-    app.get('/createTableMeeting', (req, res) => {
-        db.query(meetingTableQuery, (error, result) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("An error occurred while creating the inviteeTableQuery.");
-                return;
-            }
-        console.log('Meeting Table created', result);
-        res.send(result);
-        })
+    db.query(meetingTableQuery, (err) => {
+    if(err) throw err
+        console.log('Meeting Table created');
+        });    
+
+// Insert values into Invitee
+
+const insertValuesInvitee = `INSERT INTO Invitee (invitee_name, invitee_by) 
+    VALUES 
+    ('PERSON_1', 'COMPANY_1'), 
+    ('PERSON_2', 'COMPANY_2'),
+    ('PERSON_3', 'COMPANY_3'), 
+    ('PERSON_4', 'COMPANY_4'), 
+    ('PERSON_5', 'COMPANY_5')`;
+
+    db.query(insertValuesInvitee, (err) => {
+        if(err) throw err 
+            console.log('Invitee values added');
         });
 
+        // Insert values into Room
+
+const insertValuesRoom = `INSERT INTO Room (room_name, floor_number)
+    VALUES 
+    ('ROOM_1', 1), 
+    ('ROOM_2', 2), 
+    ('ROOM_3', 3), 
+    ('ROOM_4', 4), 
+    ('ROOM_5', 5)`;
+
+    db.query(insertValuesRoom, (err) => {
+    if(err) throw err
+        console.log('Room values added');
+        });
 
 // Insert values into Meeting
-
-    const insertValuesMeeting = `INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no)
-    VALUES ('MEETING_1', '09:00:00', '10:00:00', 1), ('MEETING_2', '10:00:00', '11:00:00', 2), 
-    ('MEETING_3', '11:00:00', '12:00:00', 3), ('MEETING_4', '12:00:00', '13:00:00', 4), 
+const insertValuesMeeting = `INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no)
+    VALUES 
+    ('MEETING_1', '09:00:00', '10:00:00', 1), 
+    ('MEETING_2', '10:00:00', '11:00:00', 2), 
+    ('MEETING_3', '11:00:00', '12:00:00', 3), 
+    ('MEETING_4', '12:00:00', '13:00:00', 4), 
     ('MEETING_5', '13:00:00', '14:00:00', 5)`;
 
-    app.get('/insertValuesMeeting', (req, res) => {
-        db.query(insertValuesMeeting, (error, result) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("An error occurred while inserting the values.");
-                return;
-            }
-        console.log('Meeting values added', result);
-        res.send(result);
+    db.query(insertValuesMeeting, (err) => {
+    if(err) throw err
+        console.log('Meeting values added');
         });
-    })
 
+//Show table Invitee
+const selectInveeeTable = `SELECT * FROM Invitee`;
+    db.query(selectInveeeTable, (err, result) => {
+        if(err) throw err 
+            console.log('Table invitee retrieved');
+            console.log(result); // Display the retrieved data
+        });
+
+// Show table Room
+const selectRoomQuery = `SELECT * FROM Room`;
+    db.query(selectRoomQuery, (err,result) => {
+    if (err) throw err
+        console.log('Table room retrieved');
+        console.log(result); // Display the retrieved data
+        });
+    
 //Show table Meeting
+const selectQuery = `SELECT * FROM Meeting`;
+db.query(selectQuery, (err, result) => {
+    if (err) throw err;
+    console.log('Table meeting retrieved');
+    console.log(result); // Display the retrieved data
+});
 
-    app.get('/showTableMeeting', (req, res) => {
-        const selectQuery = `SELECT * FROM Meeting`;
-        db.query(selectQuery, (error, result) => {
-            if (error) {
-                console.log(error);
-                res.status(500).send("An error occurred while retrieving table data.");
-                return;
-            }
-            console.log('Table meeting retrieved', result);
-            res.send(result);
-        });
-    })
-
-
-app.listen(3000, () => {
-    console.log(`App listening on port ${port}`)
-})
+    db.end((err) => {
+    if (err) throw err
+        console.log("Database connection closed")
+        })
